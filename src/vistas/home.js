621204792a62c6f584972bb4bf6014a8d5ca46bf
Flor2801 <florenciasarria1@hotmail.com>
2021-06-08 18:43:08 -0300

    Commit

--- src/vistas/home.js
+++ src/vistas/home.js
@@ -1,6 +1,5 @@
 import { React, useState, useEffect } from "react";
 import Section from "../components/section";
-import Carrousel from "./carrousel";
 import { TRENDING_MOVIES, TRENDING_SERIES } from "../utils/variables.js";
 import useFetch from "../hooks.js/useFetch";
 
@@ -11,10 +10,10 @@ const Home = () => {
   const series = useFetch(TRENDING_SERIES);
 
 
+
   return (
     <>
-      <Carrousel />
-
+      
       <Section item={peliculas} title="Peliculas que son tendencia" url="/peliculas/tendencias" tipo="Peliculas" />
       <Section item={series} title="Series que son tendencia" url="/series/tendencias" tipo="Series"/> 
 
